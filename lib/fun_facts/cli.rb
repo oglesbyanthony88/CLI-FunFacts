1fc545c2d7d35e69d0f8449d80232d7f2f18fc31
Anthony Oglesby <anthonyoglesby88@gmail.com>
2019-10-08 17:18:44 -0400

    NOT WORKING

--- lib/fun_facts/cli.rb
+++ lib/fun_facts/cli.rb
@@ -1,8 +1,9 @@
-class FunFacts::CLI
+module FunFacts
+    class CLI
 
     def run
         list
-        options
+        #options
         goodbye
     end
 
@@ -12,27 +13,26 @@ class FunFacts::CLI
 
     def list
         puts "Todays Fun Facts are:"
-        @facts = FunFacts::Scrapper.today
-        @facts.each.with_index(1) do |fact, i|
-            puts "#{i}. #{fact.description}"
-        end
-
-    end
-
-    def options
-        input = nil
-        while input != "exit"
-        puts "Enter the number of the fun fact that you would like to read more about. Type exit to leave. Type list to see the Fun Facts again!"
-        input = gets.chomp.downcase
-        if input.to_i > 0 
-            Launchy.open(@facts.source[input.to_i])
-        elsif input == "list"
-            list
-        else
-            puts "That was not an option. Type list to see the Fun Facts for today or exit to leave."
+        facts = FunFacts::Scrapper.scrape_kickass
+        facts.each.with_index(1) do |fact, i|
+            puts "#{i}. #{fact}"
         end
     end
+
+    # def options
+    #     input = nil
+    #     while input != "exit"
+    #     puts "Enter the number of the fun fact that you would like to read more about. Type exit to leave. Type list to see the Fun Facts again!"
+    #     input = gets.chomp.downcase
+    #     if input.to_i > 0 
+    #         Launchy.open(@facts.source[input.to_i])
+    #     elsif input == "list"
+    #         list
+    #     else
+    #         puts "That was not an option. Type list to see the Fun Facts for today or exit to leave."
+    #     end
+    # end
+#end
 end
-    
 end
 
--- lib/fun_facts/scrapper.rb
+++ lib/fun_facts/scrapper.rb
@@ -1,20 +1,21 @@
-class FunFacts::Scrapper
-    attr_accessor :url, :doc
-    
-    facts = []
-    
-    def initialize
-        self.doc = Nokogiri::HTML(open("https://www.kickassfacts.com/fact-of-the-day/"))
+require "pry"
+module FunFacts
+    class Scrapper
+        attr_accessor :facts, :sources
+        
+        def self.scrape_kickass
+            facts = Array.new(100), index = 0
+                doc = Nokogiri::HTML(open("https://www.kickassfacts.com/fact-of-the-day/"))
+                doc.search("#content li")[index].each do |f|
+                facts[index] = f.text
+                
+                index += 1
+            end
+            facts
+        end
+
+
+
+
     end
-
-    def scrape_facts
-
-        facts << self.scrap_facts
-        binding.pry
-    end
-    
-
-
-
-
 end
